df96090d084269a228d6b959a9376f40eebbcf75
Gabriel Yoshino <85001629+Gabrielhyds@users.noreply.github.com>
2022-06-14 18:18:10 -0300

    banco atualizado

--- Banco/pizzaria.sql
+++ Banco/pizzaria.sql
@@ -16,11 +16,13 @@ CREATE TABLE usuario(
   	genero varchar(20) DEFAULT NULL,
   	cpf char(11) DEFAULT NULL,
   	salario decimal(7,2) DEFAULT NULL,
-  	foto varchar(255) DEFAULT NULL
 );
 
-INSERT INTO `usuario` (`idUsuario`, `nome`, `permissao`, `senha`, `usuario`, `genero`, `cpf`, `salario`, `foto`) VALUES
-(4, 'gabriel ', '1', '123456', 'Gabriel', 'Masculino', '1234567890', '1000.00', 'ee45c748d50bbd63773e9c51772551a1.png');
+-- inserindo dados na tabela usuário
+INSERT INTO `usuario` (`idUsuario`, `nome`, `permissao`, `senha`, `usuario`, `genero`, `cpf`, `salario`) VALUES
+(6, 'jose ', '2', '654321', 'Jose123', 'Masculino', '1234567890', '1000.00');
+
+-- pesquisando os usuários.
 SELECT * FROM usuario
 
 -- *
@@ -37,31 +39,13 @@ CREATE TABLE telefone(
 );
 
 -- *
--- Estrutura da tabela Endereco. 
--- *
-
-CREATE TABLE endereco(
-  	id int NOT NULL PRIMARY KEY,
-  	cep char(9) DEFAULT NULL,
-  	logradouro varchar(100) DEFAULT NULL,
-  	bairro varchar(100) DEFAULT NULL,
-  	cidade varchar(50) DEFAULT NULL,
-  	estado char(2) DEFAULT NULL,
-  	complemento varchar(50) DEFAULT NULL,
-  	numero int(11) DEFAULT NULL,
-  	codEndereco int DEFAULT NULL,
-  	FOREIGN KEY(codEndereco) REFERENCES usuario(idUsuario)
-);
-
-
--- *
 -- Estrutura da tabela Produtos. 
 -- *
 CREATE TABLE produto (
    id int NOT NULL PRIMARY KEY,
    nome varchar(100) NOT NULL,
    descricao varchar(255) DEFAULT NULL,
-   image varchar(255) NOT NULL,
+   imagem varchar(255) NOT NULL,
    preco double(10,2) NOT NULL
 );
 
